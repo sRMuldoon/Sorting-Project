e80b06d16de269d12bd0a9efccebf565522c4d0c
069986115 <069986115@109R030573XJQ22.EDU.YRDSB.NET>
2017-11-30 12:03:14 -0500

    Made selection sort method more efficient in terms of complexity

--- src/sorting/Selection.java
+++ src/sorting/Selection.java
@@ -14,25 +14,30 @@ public class Selection {
      * Method to sort a set of data by finding the minimum value in the set repeatedly
      * (selection sort)
      * @param toSort array of Comparable objects to be sorted
-     * @param smallToLarge boolean determining the order of the sorted array;
+     * @param smallToLarge Boolean object determining the order of the sorted array;
      *  if true, return smallest to largest, else return largest to smallest
      * @return sorted form of the parameter array
      */
-    public static Comparable [] sort(Comparable [] toSort, boolean smallToLarge){
-        //Initialize return variable using length of parameter
-        Comparable [] temp = new Comparable [toSort.length];
+    public static Comparable [] sort(Comparable [] toSort, Boolean smallToLarge){
+        //Initialize variable to store index of min/max
+        int temp = 0;
         //Initialize comparison variable to first element of array
         Comparable comp = toSort[0];
+        //Convert Boolean to positive/negative int modifier
+        int mod = smallToLarge.compareTo(!smallToLarge);
         //Use nested for loops to repeatedly find min or max of data for array's length
-        for(int i = 0; i<toSort.length; i++){
-            for(int j = 0; j<toSort.length; j++)
+        for(int i=(toSort.length-1)*-smallToLarge.compareTo(true); (smallToLarge && i<toSort.length) || (!smallToLarge && i>0); i+=mod){
+            for(int j = i; (smallToLarge && j<toSort.length) || (!smallToLarge && j>0); j+=mod)
                 //Use XOR and boolean parameters to handle both directions of sorting
-                if(comp.compareTo(toSort[j])<0^smallToLarge)
+                if(comp.compareTo(toSort[j])<0^smallToLarge){
                     comp = toSort[j];
+                    temp=j;
+                }
             //Set return variable to comparison variable
-            temp[i]=comp;
+            toSort[temp]=toSort[i];
+            toSort[i]=comp;
         }
-        System.out.println(temp[0]);
-        return temp;
+        System.out.println(toSort[0]);
+        return toSort;
     }
 }
